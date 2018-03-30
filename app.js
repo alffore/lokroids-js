3857fb90f1f9f9b37a7a619fdbffd8f52bf41e64
A. Alfonso F. R <alffore@gmail.com>
2018-03-30 13:38:00 -0600

    Ajustes y pruebas app dinamica, datos json y contenido estático

--- app.js
+++ app.js
@@ -1,74 +1,56 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
+var express = require('express')
+var bodyParser = require('body-parser')
+var path = require('path')
 
+var app = express()
 
-var app = express();
-
-
-//middleware
-/*var logger = function(req, res, next) {
+// middleware
+/* var logger = function(req, res, next) {
     console.log('Logging...');
     next();
 }
 
-app.use(logger);*/
+app.use(logger); */
 
+// view engine
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'vistas'))
 
-//view engine
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'vistas'));
+// Body Parser Middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
+// recursos estaticos
+app.use(express.static(path.join(__dirname, 'publico')))
 
-//Body Parser Middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+// routing
+// puro contenido dinamico
 
-//recursos estaticos
-app.use(express.static(path.join(__dirname, 'publico')));
+app.get('/salest', function (req, res) {
+  res.send('Hola mundo')
+})
 
+// routing
+// JSON
+var persona = {
+  nombre: 'A. Alfonso F. R.',
+  edad: 45
+}
 
+app.get('/json', function (req, res) {
+  res.json(persona)
+})
 
+// routing contenido dinamico templates
+app.get('/', function (req, res) {
+  res.render('index', { titulo: ' Hola Mundo' })
+})
 
-//routing
-//puro contenido dinamico
-/*app.get('/', function(req, res) {
+app.post('/clasi/ajusta', function (req, res) {
+  console.log(req.body.nombre)
+  res.render('index', { titulo: 'Entrada: ' + req.body.nombre })
+})
 
-    res.send('Hola mundo');
-
-});*/
-
-
-//routing 
-//JSON
-/*var persona = {
-    nombre: 'A. Alfonso F. R.',
-    edad: 45
-};
-
-app.get('/', function(req, res) {
-
-    res.json(persona);
-
-
-});*/
-
-
-//routing contenido dinamico templates
-app.get('/', function(req, res) {
-
-    res.render('index', { titulo: " Hola Mundo" });
-
-});
-
-
-app.post('/clasi/ajusta', function(req, res) {
-
-    console.log(req.body.nombre);
-    res.render('index', { titulo: "Entrada: " + req.body.nombre });
-});
-
-
-app.listen(3000, function() {
-    console.log("escucha 3000");
-});
+app.listen(3000, function () {
+  console.log('escucha 3000')
+})
