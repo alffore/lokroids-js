3b02e05c791dfd094f5a953905b230903027e99e
A. Alfonso F. R <alffore@gmail.com>
2018-03-31 22:18:45 -0600

    Se agregan la funcionalidad básica, para recuperry pintar las imagenes para clasificación humana

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var path = require('path')
 var app = express()
 
 // view engine
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'vistas'))
+//app.set('view engine', 'ejs')
+//app.set('views', path.join(__dirname, 'vistas'))
 
 // Body Parser Middleware
 app.use(bodyParser.json())
@@ -18,31 +18,48 @@ app.use(express.static(path.join(__dirname, 'publico')))
 // routing
 // puro contenido dinámico
 
-app.get('/salest', function (req, res) {
-  res.send('Hola mundo')
+app.get('/salest', function(req, res) {
+    res.send('Hola mundo')
 })
 
 // routing
 // JSON
 var persona = {
-  nombre: 'A. Alfonso F. R.',
-  edad: 45
+    nombre: 'A. Alfonso F. R.',
+    edad: 45
 }
 
-app.get('/json', function (req, res) {
-  res.json(persona)
+app.get('/json', function(req, res) {
+    res.json(persona)
 })
 
 // routing contenido dinamico templates
-app.get('/dinamico', function (req, res) {
-  res.render('index', { titulo: ' Hola Mundo' })
+/*app.get('/dinamico', function(req, res) {
+    res.render('index', { titulo: ' Hola Mundo' })
 })
 
-app.post('/clasi/ajusta', function (req, res) {
-  console.log(req.body.nombre)
-  res.render('index', { titulo: 'Entrada: ' + req.body.nombre })
+app.post('/clasi/ajusta', function(req, res) {
+    guardaClasifica(req);
+
+    res.render('index', { titulo: 'Entrada: ' + req.body.nombre })
+
+})*/
+
+app.get('/mestado/:id', (req, res) => {
+    guardaClasifica(req, res)
+
 })
 
-app.listen(3000, function () {
-  console.log('escucha 3000')
+app.listen(3000, function() {
+    console.log('escucha 3000')
 })
+
+
+function guardaClasifica(req, res) {
+
+    var aux = req.params.id.split("_")
+    console.log(aux)
+
+
+    res.json(persona)
+}
--- publico/app_clasi.js
+++ publico/app_clasi.js
@@ -1,46 +1,52 @@
 const main = document.querySelector('main')
 
 window.addEventListener('load', async e => {
-  actualizaLista()
+    actualizaLista()
 })
 
-async function actualizaLista () {
-  const res = await fetch(`//127.0.0.1:3000/fotos/fotos.json`)
-  const json = await res.json()
+async function actualizaLista() {
+    const res = await fetch(`//127.0.0.1:3000/imagenes/fotos.json`)
+    const json = await res.json()
 
-  main.innerHTML = json.entradas.map(creaEntrada).join('\n')
+    main.innerHTML = json.entradas.map(creaEntrada).join('\n')
 
-  const obmdes = document.querySelectorAll('.mdes')
-  const obmdor = document.querySelectorAll('.mdor')
+    const obmdes = document.querySelectorAll('.mdes')
+    const obmdor = document.querySelectorAll('.mdor')
 
-  for (var i = 0; i < obmdes.length; i++) {
-    obmdes[i].onclick = function () {
-      console.log(`click en ${this.id}`)
+    for (var i = 0; i < obmdes.length; i++) {
+        obmdes[i].onclick = function() {
+            marcaEstado(this)
+        }
+
+        obmdor[i].onclick = function() {
+            marcaEstado(this)
+        }
     }
-
-    obmdor[i].onclick = function () {
-      console.log(`click en ${this.id}`)
-    }
-  }
 }
 
-function creaEntrada (entrada) {
-  var marcades = ''
-  var marcador = ''
+function creaEntrada(entrada) {
+    var marcades = ''
+    var marcador = ''
 
-  if (entrada.clasificado === 'despierto') {
-    marcades = 'checked'
-  }
+    if (entrada.clasificado === 'despierto') {
+        marcades = 'checked'
+    }
 
-  if (entrada.clasificado === 'dormido') {
-    marcador = 'checked'
-  }
+    if (entrada.clasificado === 'dormido') {
+        marcador = 'checked'
+    }
 
-  return `<div class="article">
-          <div class="imagen_foto"><img src="${entrada.imagen_url}" width="320"></div>
+    return `<div class="article">
+          <div class="foto_id">${entrada.id}</div>
+          <div class="imagen_foto"><img src="${entrada.imagen_url}" width="240"></div>
             <div class="clasificacion">
             <input type="radio" value="despierto" name="estado${entrada.id}" class="mdes" id="mdes_${entrada.id}" ${marcades}> despierto
             <input type="radio" value="dormido" name="estado${entrada.id}" class="mdor" id="mdor_${entrada.id}" ${marcador}> dormido
             </div>
           </div>`
+}
+
+
+function marcaEstado(objEstado) {
+    fetch(`//127.0.0.1:3000/mestado/${objEstado.id}`)
 }
