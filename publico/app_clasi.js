695f6b9210178b8b43e0f8a76cc78007da79375e
A. Alfonso F. R <alffore@gmail.com>
2018-04-02 22:26:44 -0500

    Limpieza de código

--- publico/app_clasi.js
+++ publico/app_clasi.js
@@ -2,43 +2,43 @@ const url_base = '//127.0.0.1:3000/'
 const main = document.querySelector('main')
 
 window.addEventListener('load', async e => {
-    actualizaLista()
+  actualizaLista()
 })
 
-async function actualizaLista() {
-    //const res = await fetch(`${url_base}imagenes/fotos.json`)
-    const res = await fetch(`${url_base}json`)
-    const json = await res.json()
+async function actualizaLista () {
+    // const res = await fetch(`${url_base}imagenes/fotos.json`)
+  const res = await fetch(`${url_base}json`)
+  const json = await res.json()
 
-    main.innerHTML = json.entradas.map(creaEntrada).join('\n')
+  main.innerHTML = json.entradas.map(creaEntrada).join('\n')
 
-    const obmdes = document.querySelectorAll('.mdes')
-    const obmdor = document.querySelectorAll('.mdor')
+  const obmdes = document.querySelectorAll('.mdes')
+  const obmdor = document.querySelectorAll('.mdor')
 
-    for (var i = 0; i < obmdes.length; i++) {
-        obmdes[i].onclick = function() {
-            marcaEstado(this)
-        }
-
-        obmdor[i].onclick = function() {
-            marcaEstado(this)
-        }
+  for (var i = 0; i < obmdes.length; i++) {
+    obmdes[i].onclick = function () {
+      marcaEstado(this)
     }
+
+    obmdor[i].onclick = function () {
+      marcaEstado(this)
+    }
+  }
 }
 
-function creaEntrada(entrada) {
-    var marcades = ''
-    var marcador = ''
+function creaEntrada (entrada) {
+  var marcades = ''
+  var marcador = ''
 
-    if (entrada.clasificado === 'despierto') {
-        marcades = 'checked'
-    }
+  if (entrada.clasificado === 'despierto') {
+    marcades = 'checked'
+  }
 
-    if (entrada.clasificado === 'dormido') {
-        marcador = 'checked'
-    }
+  if (entrada.clasificado === 'dormido') {
+    marcador = 'checked'
+  }
 
-    return `<div class="article">
+  return `<div class="article">
           <div class="foto_id">id: ${entrada.id}</div>
           <div class="imagen_foto"><img src="${entrada.imagen_url}" width="240"></div>
             <div class="clasificacion">
@@ -48,6 +48,6 @@ function creaEntrada(entrada) {
           </div>`
 }
 
-function marcaEstado(objEstado) {
-    fetch(`//127.0.0.1:3000/mestado/${objEstado.id}`)
+function marcaEstado (objEstado) {
+  fetch(`//127.0.0.1:3000/mestado/${objEstado.id}`)
 }
