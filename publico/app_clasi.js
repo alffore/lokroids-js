d6204bf8883501d9285ddf381b4a79280d2fcdb4
A. Alfonso F. R <alffore@gmail.com>
2019-01-05 12:37:17 -0600

    Se incluye la opcion otro estado

--- publico/app_clasi.js
+++ publico/app_clasi.js
@@ -17,6 +17,7 @@ async function actualizaLista () {
 
   const obmdes = document.querySelectorAll('.mdes')
   const obmdor = document.querySelectorAll('.mdor')
+  const obmotr = document.querySelectorAll('.motr')
 
   for (var i = 0; i < obmdes.length; i++) {
     obmdes[i].onclick = function () {
@@ -24,6 +25,10 @@ async function actualizaLista () {
     }
 
     obmdor[i].onclick = function () {
+      marcaEstado(this)
+    }
+
+    obmotr[i].onclick = function () {
       marcaEstado(this)
     }
   }
@@ -45,12 +50,19 @@ function creaEntrada (entrada) {
     estilobloque = 'article_dor'
   }
 
+  if (entrada.clasificado === 'otro') {
+    marcador = 'checked'
+    estilobloque = ''
+  }
+
+
   return `<div class="${estilobloque}">
           <div class="foto_id">id: ${entrada.id}</div>
           <div class="imagen_foto"><img src="${entrada.imagen_url}" width="240"></div>
             <div class="clasificacion">
             <input type="radio" value="despierto" name="estado${entrada.id}" class="mdes" id="mdes_${entrada.nimg}_${entrada.id}" ${marcades}> despierto
             <input type="radio" value="dormido" name="estado${entrada.id}" class="mdor" id="mdor_${entrada.nimg}_${entrada.id}" ${marcador}> dormido
+            <input type="radio" value="otro" name="estado${entrada.id}" class="motr" id="motr_${entrada.nimg}_${entrada.id}" ${marcador}> otro
             </div>
           </div>`
 }
